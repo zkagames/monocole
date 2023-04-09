6a8b0219de5fa91fddf1f23ae2c7e06d52b2b380
zkagames <zkagames.42@gmail.com>
2023-04-09 09:47:24 +0300

    jest

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import {App} from '../App';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render( <BrowserRouter> 
+    <App />
+    </BrowserRouter> );
   const linkElement = screen.getByText(/App/i);
   expect(linkElement).toBeInTheDocument();
 });
